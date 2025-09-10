25dd9b48db4509f1d10d9abf761af5cd205faa62
Gastech99 <83505655+Gastech99@users.noreply.github.com>
2025-09-10 10:59:01 +0000

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,29 @@
+-- Question 1 🧑‍💼
+-- Get firstName, lastName, email, and officeCode of all employees
+SELECT e.firstName, 
+       e.lastName, 
+       e.email, 
+       e.officeCode
+FROM employees e
+INNER JOIN offices o 
+       ON e.officeCode = o.officeCode;
+
+-- Question 2 🛍️
+-- Get productName, productVendor, and productLine from products
+SELECT p.productName, 
+       p.productVendor, 
+       p.productLine
+FROM products p
+LEFT JOIN productlines pl 
+       ON p.productLine = pl.productLine;
+
+-- Question 3 📦
+-- Get orderDate, shippedDate, status, and customerNumber for first 10 orders
+SELECT o.orderDate, 
+       o.shippedDate, 
+       o.status, 
+       o.customerNumber
+FROM customers c
+RIGHT JOIN orders o 
+       ON c.customerNumber = o.customerNumber
+LIMIT 10;
